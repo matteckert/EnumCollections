3dfbca589eec3da1991f8ea3b324ab1f0a962626
Matt Eckert <me@matteckert.com>
2016-09-21 18:54:39 -0500

    Add test project to solution

---
+++ EnumCollections.Tests/EnumSetTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace EnumCollections.Tests
+{
+    [TestFixture]
+    public class EnumSetTests
+    {
+        public enum EmptyEnum
+        {
+        }
+
+        [Test]
+        public void CanCreateEnumSetWithNoElements()
+        {
+            var enumSet = EnumSet<EmptyEnum>.Of<EmptyEnum>();
+            Assert.IsInstanceOf<EnumSet<EmptyEnum>>(enumSet);
+        }
+    }
+}
