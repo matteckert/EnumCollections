ee88e7f0baf5f4838af49f695f38f8ef0e9f1601
Matt Eckert <me@matteckert.com>
2016-09-28 05:35:09 -0500

    Refactor tests to use Of

--- EnumCollections.Tests/EnumSetTests.cs
+++ EnumCollections.Tests/EnumSetTests.cs
@@ -9,174 +9,105 @@ namespace EnumCollections.Tests
         [TestFixture]
         public class GivenAnEmptyEnum
         {
-            private readonly EnumSet<E> _enumSet;
-
-            public enum E
-            {
-            }
-
-            public GivenAnEmptyEnum()
-            {
-                _enumSet = EnumSet.Of<E>();
-            }
+            public enum E { }
 
             [Test]
             public void WhenCreated_ShouldBeAnEnumSet()
             {
-                Assert.IsInstanceOf<EnumSet<E>>(_enumSet);
+                Assert.IsInstanceOf<EnumSet<E>>(EnumSet.Of<E>());
             }
 
             [Test]
             public void WhenCompared_ThenShouldBeEqual()
             {
-                CollectionAssert.AreEqual(_enumSet, EnumSet.Of<E>());
+                CollectionAssert.AreEqual(EnumSet.Of<E>(), EnumSet.Of<E>());
             }
 
             [Test]
             public void WhenComparedToNullSet_ThenShouldThrow()
             {
-                Assert.Throws<ArgumentNullException>(() => _enumSet.SetEquals(null));
+                Assert.Throws<ArgumentNullException>(() => EnumSet.Of<E>().SetEquals(null));
             }
         }
 
         [TestFixture]
         public class GivenAnEnumWithElements
         {
-            private EnumSet<E> _enumSet;
-
-            public enum E
-            {
-                A,
-                B,
-                C
-            }
-
-            private void AssertContains(E element)
-            {
-                Assert.IsTrue(_enumSet.Contains(element));
-            }
-
-            [SetUp]
-            public void Init()
-            {
-                _enumSet = EnumSet.Of<E>();
-            }
+            public enum E { A, B, C }
 
             [Test]
             public void WhenCreated_ThenShouldHaveNoElements()
             {
-                CollectionAssert.IsEmpty(_enumSet);
+                CollectionAssert.IsEmpty(EnumSet.Of<E>());
             }
 
             [Test]
             public void WhenCreated_ThenCountShouldBeZero()
             {
-                Assert.IsTrue(_enumSet.Count == 0);
+                Assert.IsTrue(EnumSet.Of<E>().Count == 0);
             }
 
             [Test]
             public void WhenCreated_ThenShouldNotContainElement()
             {
-                Assert.IsFalse(_enumSet.Contains(E.B));
-                CollectionAssert.DoesNotContain(_enumSet, E.B);
+                Assert.IsFalse(EnumSet.Of<E>().Contains(E.B));
             }
 
             [Test]
             public void WhenOneElement_ThenCountShouldBeOne()
             {
-                _enumSet.Add(E.B);
-                Assert.IsTrue(_enumSet.Count == 1);
+                Assert.IsTrue(EnumSet.Of(E.B).Count == 1);
             }
 
             [Test]
             public void WhenOneElement_ThenShouldContainElement()
             {
-                _enumSet.Add(E.B);
-                AssertContains(E.B);
+                Assert.IsTrue(EnumSet.Of(E.B).Contains(E.B));
             }
 
             [Test]
             public void WhenMultipleElements_ThenCountShouldBeNumberOfElements()
             {
-                _enumSet.Add(E.B);
-                _enumSet.Add(E.C);
-                Assert.IsTrue(_enumSet.Count == 2);
+                Assert.IsTrue(EnumSet.Of(E.B, E.C).Count == 2);
             }
 
             [Test]
             public void WhenMultipleElements_ThenShouldContainAllElements()
             {
-                _enumSet.Add(E.B);
-                _enumSet.Add(E.C);
-                AssertContains(E.B);
-                AssertContains(E.C);
+                Assert.IsTrue(EnumSet.Of(E.B, E.C).Contains(E.B));
+                Assert.IsTrue(EnumSet.Of(E.B, E.C).Contains(E.C));
             }
 
             [Test]
             public void WhenNotAllElements_ThenShouldNotContainElementsNotAdded()
             {
-                _enumSet.Add(E.B);
-                _enumSet.Add(E.C);
-                CollectionAssert.DoesNotContain(_enumSet, E.A);
+                Assert.IsFalse(EnumSet.Of(E.B, E.C).Contains(E.A));
             }
         }
 
         [TestFixture]
         public class GivenTwoEnums
         {
-            private EnumSet<E> _a;
-            private EnumSet<E> _b;
-
-            public enum E
-            {
-                A,
-                B,
-                C
-            }
-
-            public class AClass {}
-
-
-            [SetUp]
-            public void Init()
-            {
-                _a = EnumSet.Of<E>();
-                _b = EnumSet.Of<E>();
-            }
-
-            [Test]
-            public void WhenEmpty_ThenEqual()
-            {
-                Assert.IsTrue(_a.SetEquals(_b));
-            }
+            public enum E { A, B, C }
 
             [Test]
             public void WhenSameElements_ThenEqual()
             {
-                _a.Add(E.A);
-                _b.Add(E.A);
-                Assert.IsTrue(_a.SetEquals(_b));
+                Assert.IsTrue(EnumSet.Of(E.A).SetEquals(EnumSet.Of(E.A)));
             }
 
             [Test]
             public void WhenDifferentElements_ThenNotEqual()
             {
-                _a.Add(E.A);
-                _b.Add(E.B);
-                Assert.IsFalse(_a.SetEquals(_b));
+                Assert.IsFalse(EnumSet.Of(E.A).SetEquals(EnumSet.Of(E.B)));
             }
 
             [Test]
             public void WhenSameAndDifferentElements_ThenSymmetricExceptWithGivesDifferences()
             {
-                var expected = EnumSet.Of(E.A, E.C);
-
-                _a.Add(E.A);
-                _a.Add(E.B);
-                _b.Add(E.B);
-                _b.Add(E.C);
-                _a.SymmetricExceptWith(_b);
-                Assert.IsTrue(_a.SetEquals(expected));
+                var a = EnumSet.Of(E.A, E.B);
+                a.SymmetricExceptWith(EnumSet.Of(E.B, E.C));
+                Assert.IsTrue(a.SetEquals(EnumSet.Of(E.A, E.C)));
             }
         }
     }
